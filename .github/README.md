df7ad0f272ea961408fd97c32c0425fa977922d7
Daniel Burger <public@danielburger.online>
2021-09-09 14:51:55 +0200

    Adjusting the eslintrc for mr-client

--- .github/README.md
+++
@@ -1,3 +0,0 @@
-# GitHub
-
-GitHub templates and workflows.
---
+++ packages/mr-client/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'next',
+    'next/core-web-vitals',
+    'prettier'
+  ],
+  globals: {
+    React: true,
+    JSX: true,
+    context: true
+  },
+  ignorePatterns: ['.eslintrc.js']
+}
