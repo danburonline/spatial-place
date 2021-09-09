693601d102218d2e565c543409fe9be6996c8205
Daniel Burger <public@danielburger.online>
2021-09-09 15:48:18 +0200

    Adding a Storybook setup

--- packages/mr-storybook/stories/Header.tsx
+++ packages/mr-storybook/stories/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Button } from './Button'
 import './header.css'
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
@@ -31,21 +30,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         </svg>
         <h1>Acme</h1>
       </div>
-      <div>
-        {user ? (
-          <Button size='small' onClick={onLogout} label='Log out' />
-        ) : (
-          <>
-            <Button size='small' onClick={onLogin} label='Log in' />
-            <Button
-              primary
-              size='small'
-              onClick={onCreateAccount}
-              label='Sign up'
-            />
-          </>
-        )}
-      </div>
+      <div></div>
     </div>
   </header>
 )
