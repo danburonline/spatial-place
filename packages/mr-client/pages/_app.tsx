9fd1916cdaab6af1f3cb1e8dacfb260347448e38
Daniel Burger <public@danielburger.online>
2021-09-11 17:13:34 +0200

    Working example for TypeDoc for Nest.js

--- packages/mr-client/pages/_app.tsx
+++ packages/mr-client/pages/_app.tsx
@@ -1,5 +1,16 @@
 import '../styles/globals.css'
 
+/**
+ * Custom App component for Next.js
+ *
+ * @remarks
+ * The name of this file is an official convention of Next.js.
+ *
+ * @param Component - The wrapper component
+ * @param pageProps - All page props for the rendered children
+ * @returns JSX.Element
+ *
+ */
 function MindRacingClient({ Component, pageProps }): JSX.Element {
   return <Component {...pageProps} />
 }
