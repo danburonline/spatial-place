f3e98beb4a7dc1875f7a494cc04e6bb5944a0a2d
Daniel Burger <public@danielburger.online>
2021-09-24 12:29:09 +0200

    Changing the TypeDoc settings

--- packages/mr-server/src/main.ts
+++ packages/mr-server/src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
 
 import { AppModule } from './app.module'
 
@@ -10,7 +12,8 @@ import { AppModule } from './app.module'
  *
  */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.useStaticAssets(join(__dirname, '..', 'docs'))
   await app.listen(3000)
 }
 bootstrap()
