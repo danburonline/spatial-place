d9167fd0388cc265b22d133a2a3499d07270f181
Daniel Burger <public@danielburger.online>
2021-09-28 15:51:31 +0200

    Small *.stories refactor

--- packages/mr-client/components/scenes/MainGameScene/MainGameScene.tsx
+++ packages/mr-client/components/scenes/MainGameScene/MainGameScene.tsx
@@ -2,7 +2,7 @@ import { Loader, OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 
-import Labyrinth from '../../models/Labyrinth'
+import Labyrinth from '../../models/Labyrinth/Labyrinth'
 import Lighting from './components/Lighting'
 
 export default function GameScene(): JSX.Element {
