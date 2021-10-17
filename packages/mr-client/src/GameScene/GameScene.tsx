068b36769e0ea19ab4790ffdceafbfd4a6c50dbc
Daniel Burger <public@danielburger.online>
2021-10-17 16:23:26 +0200

    Add physics scene setup

--- packages/mr-client/src/GameScene/GameScene.tsx
+++ packages/mr-client/src/GameScene/GameScene.tsx
@@ -7,16 +7,25 @@ import Floor from '../Floor/Floor'
 import Labyrinth from '../Labyrinth/Labyrinth'
 import Lighting from '../Lighting/Lighting'
 import Player from '../Player/Player'
+import Colliders from './components/Colliders'
 
 export default function GameScene(): JSX.Element {
   return (
     <>
       <Canvas frameloop='demand' camera={{ position: [0, 20, 0] }}>
         <Suspense fallback={null}>
-          <Physics>
+          <Physics
+            allowSleep={true}
+            gravity={[0, -20, 0]}
+            tolerance={0.0001}
+            size={115}
+            broadphase={'SAP'}
+            iterations={10}
+          >
             <Floor />
             <Labyrinth />
             <Player />
+            <Colliders />
           </Physics>
         </Suspense>
         <Lighting />
