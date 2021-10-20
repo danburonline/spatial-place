6d6ec51628ce78ebe77a359341d4178f5eb199b2
Daniel Burger <public@danielburger.online>
2021-10-20 16:14:30 +0200

    Add `<Link/>` component around `<a/>`

--- packages/mr-client/pages/index.tsx
+++ packages/mr-client/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useUser } from '@auth0/nextjs-auth0'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home(): JSX.Element {
   const { user, isLoading, error } = useUser()
@@ -27,34 +28,29 @@ export default function Home(): JSX.Element {
             <h1 className='mb-3 text-3xl font-bold'>Welcome, {user.name}</h1>
             <h2 className='max-w-md'>
               Be sure to have a connected{' '}
-              <a
-                className='underline'
-                rel='noreferrer'
-                target='_blank'
-                href='https://iduntechnologies.ch/guardian-dev-kit'
-              >
-                IDUN Guardian
-              </a>{' '}
+              <Link href='https://iduntechnologies.ch/guardian-dev-kit'>
+                <a className='underline' rel='noreferrer' target='_blank'>
+                  IDUN Guardian
+                </a>
+              </Link>{' '}
               brain-computer interface. Click the connect button below to go to
               the setup process. Otherwise, you&apos;re good to play the game.
             </h2>
             <div className='mt-8'>
-              <a
-                className='px-4 py-2 mx-2 text-black bg-white rounded-full'
-                href='/connect'
-              >
-                Connect device
-              </a>
-              <a
-                className='px-4 py-2 mx-2 text-white bg-black rounded-full'
-                href='/start'
-              >
-                Start game
-              </a>
+              <Link href='/connect'>
+                <a className='px-4 py-2 mx-2 text-black bg-white rounded-full'>
+                  Connect device
+                </a>
+              </Link>
+              <Link href='/start'>
+                <a className='px-4 py-2 mx-2 text-white bg-black rounded-full'>
+                  Start game
+                </a>
+              </Link>
             </div>
-            <a className='mt-8 opacity-50' href='/api/auth/logout'>
-              Logout
-            </a>
+            <Link href='/api/auth/logout'>
+              <a className='mt-8 opacity-50'>Logout</a>
+            </Link>
           </>
         ) : (
           <>
@@ -63,28 +59,23 @@ export default function Home(): JSX.Element {
               Mind-controlled multiplayer 3D racing game to demonstrate the
               possibilities of the Internet of Humans and the underlying
               brain/cloud interface architecture as part of{' '}
-              <a
-                className='underline'
-                rel='noreferrer'
-                target='_blank'
-                href='https://thesis.mind.racing'
-              >
-                Daniel Burger&apos;s bachelor thesis.
-              </a>
+              <Link href='https://thesis.mind.racing'>
+                <a className='underline' rel='noreferrer' target='_blank'>
+                  Daniel Burger&apos;s bachelor thesis.
+                </a>
+              </Link>
             </h2>
             <div className='mt-12'>
-              <a
-                className='px-4 py-2 mx-2 text-black bg-white rounded-full'
-                href='/api/auth/login'
-              >
-                Login
-              </a>
-              <a
-                className='px-4 py-2 mx-2 text-white bg-black rounded-full'
-                href='/api/auth/login'
-              >
-                Create an account
-              </a>
+              <Link href='/api/auth/login'>
+                <a className='px-4 py-2 mx-2 text-black bg-white rounded-full'>
+                  Login
+                </a>
+              </Link>
+              <Link href='/api/auth/login'>
+                <a className='px-4 py-2 mx-2 text-white bg-black rounded-full'>
+                  Create an account
+                </a>
+              </Link>
             </div>
           </>
         )}
